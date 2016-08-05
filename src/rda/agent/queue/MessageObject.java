165519bcf808b1192130e6372104a3f81cf6b825
kaeru <kaeru@kaeru-PC>
2016-08-06 02:19:59 +0900

    PC Commit

--- src/rda/agent/queue/MessageObject.java
+++ src/rda/agent/queue/MessageObject.java
@@ -1,15 +1,12 @@
 package rda.agent.queue;
 
-import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
+import java.io.Serializable;
 import java.util.List;
 
 import rda.manager.IDManager;
 import rdarank.agent.rank.manager.RankAgentManager;
 
-public class MessageObject implements Externalizable {
+public class MessageObject implements Serializable {
     public Long lateTime;
     public String id;
     public List data;
@@ -21,6 +18,7 @@ public class MessageObject implements Externalizable {
         this.data = (List)data;
     }
     
+    /*
     @Override
     public void writeExternal(ObjectOutput out) throws IOException {
         out.writeObject(lateTime);
@@ -34,7 +32,7 @@ public class MessageObject implements Externalizable {
         this.id = (String) in.readObject();
         this.data = (List) in.readObject();
     }
-    
+    */
     //Only RankAgent
     public String getID(){
         IDManager rankID = RankAgentManager.getInstance().getIDManager();
