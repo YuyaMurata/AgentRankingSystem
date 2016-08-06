8433d70420ce353d5b96bb6856cb50737f5a4964
kaeru <kaeru@kaeru-PC>
2016-08-07 00:46:52 +0900

    PC Commit

--- setting/check_server.sh
+++ setting/check_server.sh
@@ -2,6 +2,11 @@
 for i in `seq 1 $1`
 do
     #確認
+    sshpass -p 11m35584 ssh h${i}@h${i} 'echo `hostname`'
+    echo "確認後 Enter"
+    read
+
+    #確認
     sshpass -p 11m35584 ssh h${i}@h${i} \
         "source /etc/profile;\
         cd $CETA_HOME/App/AgentSystem/AgentRankingSystem/setting;\
--- setting/remote-killall.sh
+++ setting/remote-killall.sh
@@ -1,12 +1,7 @@
 
 for i in `seq 1 $1`
 do
-    #確認
-    sshpass -p 11m35584 ssh h${i}@h${i} 'echo `hostname`'
-    echo "確認後 Enter"
-    read
-
-    #遠隔で実験用スクリプトを起動
+    #遠隔でサーバーを停止
     sshpass -p 11m35584 ssh h${i}@h${i} \
         "source /etc/profile;\
         cd $CETA_HOME/App/AgentSystem/AgentRankingSystem/setting;\
