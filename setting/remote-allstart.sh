70ffe11175d081056f453dcf2754d1f07420cb3b
kaeru <kaeru@kaeru-PC>
2016-08-06 17:40:01 +0900

    PC Commit

--- setting/remote-allstart.sh
+++ setting/remote-allstart.sh
@@ -4,9 +4,8 @@ echo "確認後 Enter"
 read
 
 #遠隔で実験用スクリプトを起動
-sshpass -p 11m35584 ssh $1@$1 \
+sshpass -p 11m35584 ssh root@$1 \
 "source /etc/profile;\
-../.bash_profile;\
 echo $CETA_HOME"
 #cd $CETA_HOME/App/AgentSystem/AgentRankingSystem/setting;\
 #pwd;\
