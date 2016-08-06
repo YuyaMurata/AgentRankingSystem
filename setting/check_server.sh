37c550017fc1840f886433434b8bd8ff654ee6de
kaeru <kaeru@kaeru-PC>
2016-08-07 01:13:21 +0900

    PC Commit

--- setting/check_server.sh
+++ setting/check_server.sh
@@ -2,9 +2,11 @@
 for i in `seq 1 $1`
 do
     #確認
+    echo " -------------------  Server  ---------------------------"
+    echo ""
     sshpass -p 11m35584 ssh h${i}@h${i} 'echo `hostname`'
-    echo "確認後 Enter"
-    read
+    echo ""
+    echo " --------------------------------------------------------"
 
     #確認
     sshpass -p 11m35584 ssh h${i}@h${i} \
