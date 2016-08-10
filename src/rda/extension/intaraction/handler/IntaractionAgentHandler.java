20da7ffa642ff32e436cdb190ff0e2d6b7874208
kaeru <kaeru@kaeru-PC>
2016-08-11 00:24:09 +0900

    PC Commit

--- src/rda/extension/intaraction/handler/IntaractionAgentHandler.java
+++ src/rda/extension/intaraction/handler/IntaractionAgentHandler.java
@@ -29,7 +29,7 @@ public class IntaractionAgentHandler extends MessageHandler{
         while(!AgentIntaractionExtension.getInstance().transport(trans) 
                     && SystemManagerExtension.getInstance().getState()){
             try{
-                Thread.sleep(10L);
+                Thread.sleep(1L);
             }catch(InterruptedException e){
             }
             //System.out.println("Transport Int : "+trans.toString());
