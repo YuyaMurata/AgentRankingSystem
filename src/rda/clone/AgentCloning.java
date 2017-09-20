af518d2cd64edbd5a04f5190c9725b5c6e77cc23
kaeru <kaeru@LAPTOP-JLUFA49E>
2017-09-20 12:16:32 +0900

    notePC

--- src/rda/clone/AgentCloning.java
+++ src/rda/clone/AgentCloning.java
@@ -4,36 +4,41 @@ import rda.manager.AgentManager;
 import rda.manager.IDManager;
 
 public class AgentCloning {
-    public static String cloning(AgentManager manager, String sourceID, Object originalState){
-        if(!manager.getAutoMode()) return "";
-        
-        System.out.println(">> Start Agent Cloning");
-        
-        IDManager id = manager.getIDManager();
-        String originalID = id.getOrigID(sourceID);
-        
-        //Clone
-        String cloneID = manager.createCloneAgent(originalID, originalState);
-        id.regID(originalID, cloneID);
-        
-        System.out.println(">> Agent Cloning New Copy From "+ originalID);
-        
-        return cloneID;
-    }
-    
-    public static String delete(AgentManager manager, String deleteID){
-        if(!manager.getAutoMode()) return "";
-        
-        System.out.println(">> Start Agent Delete");
-        
-        IDManager id = manager.getIDManager();
-        String originalID = id.getOrigID(deleteID);
-        
-        //Delete
-        id.deleteID(originalID, deleteID);
-        
-        System.out.println(">> Agent Cloning Delete "+ deleteID);
-        
-        return deleteID;
-    }
+
+	public static String cloning(AgentManager manager, String sourceID, Object originalState) {
+		if (!manager.getAutoMode()) {
+			return "";
+		}
+
+		System.out.println(">> Start Agent Cloning");
+
+		IDManager id = manager.getIDManager();
+		String originalID = id.getOrigID(sourceID);
+
+		//Clone
+		String cloneID = manager.createCloneAgent(originalID, originalState);
+		id.regID(originalID, cloneID);
+
+		System.out.println(">> Agent Cloning New Copy From " + originalID);
+
+		return cloneID;
+	}
+
+	public static String delete(AgentManager manager, String deleteID) {
+		if (!manager.getAutoMode()) {
+			return "";
+		}
+
+		System.out.println(">> Start Agent Delete");
+
+		IDManager id = manager.getIDManager();
+		String originalID = id.getOrigID(deleteID);
+
+		//Delete
+		id.deleteID(originalID, deleteID);
+
+		System.out.println(">> Agent Cloning Delete " + deleteID);
+
+		return deleteID;
+	}
 }
